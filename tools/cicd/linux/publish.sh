99fd4624858010fc0e853f5898e2a0224500e5fa
George Bekiaris <geobekiaris@gmail.com>
2020-02-04 13:37:42 +1100

    fix: pubish.sh script for linux

--- tools/cicd/linux/publish.sh
+++ tools/cicd/linux/publish.sh
@@ -4,10 +4,10 @@
 
 python -m pip install twine
 
-for whl in wheels/*.whl; do
+for whl in wheels/*manylinux*.whl; do
     python -m twine check "${whl}"
 done
 
-for whl in wheels/*.whl; do
+for whl in wheels/*manylinux*.whl; do
     python -m twine upload "${whl}"
 done
