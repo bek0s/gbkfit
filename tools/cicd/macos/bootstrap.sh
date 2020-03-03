e5048476a2bb88fc1430348b46a8127ebb28acf1
George Bekiaris <geobekiaris@gmail.com>
2020-03-03 19:58:08 +0800

    apple is the most depressing company ever...

--- tools/cicd/macos/bootstrap.sh
+++ tools/cicd/macos/bootstrap.sh
@@ -10,12 +10,11 @@ PYTHON_VERSION="${1//./}"
 git submodule sync --recursive
 git submodule update --init --recursive
 
-export MACOSX_DEPLOYMENT_TARGET=10.8
-
 brew update
 brew upgrade
 brew install pyenv
 
+export MACOSX_DEPLOYMENT_TARGET=10.9
 pyenv install "${PYTHON_VERSIONS[$PYTHON_VERSION]}"
 "$HOME"/.pyenv/versions/"${PYTHON_VERSIONS[$PYTHON_VERSION]}"/bin/python -m venv venv
 . venv/bin/activate
